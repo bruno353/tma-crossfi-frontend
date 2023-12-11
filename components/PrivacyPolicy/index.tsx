23b7fa4b890d2b86d3c91631ebb12da41674e49b
Bruno Santos <brunolsantos152@gmail.com>
2023-12-11 14:02:31 -0300

    feat: privacy-polici google

--- components/PrivacyPolicy/index.tsx
+++ components/PrivacyPolicy/index.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 const PrivacyPolicy = () => {
   return (
-    <section className="p-[50px]">
+    <section className="mt-[100px] p-[50px] pb-[200px]">
       <iframe
         src="https://www.termsfeed.com/live/6352531e-db26-45a1-b89d-3bd3d9ea87a9"
         width="100%"
@@ -10,6 +11,49 @@ const PrivacyPolicy = () => {
       >
         Your browser does not support Iframe
       </iframe>
+      <div className="mt-[50px] bg-[#fff] p-[10px] text-[20px] text-[#000]">
+        <div className="text-[24px]">Google OAuth privacy-policy</div>
+        <div className="mt-[20px]">
+          Effective Date: 12/11/2023 <br /> 1. Introduction This Privacy Policy
+          outlines how we collect, use, and safeguard the information of users
+          who access our application ("we," "us," or "our"). <br />
+          We are committed to protecting your privacy and ensuring that your
+          personal information is used only for the purposes outlined in this
+          policy. <br />
+          2. Information Collected We use Google Authentication to allow users
+          to access our application securely. When you log in using Google
+          Authentication, we collect the following information: Your email
+          address We collect this information solely for the purpose of
+          verifying your identity and ensuring that you have access to your
+          account. <br />
+          3. Use of Information We use the information collected for the
+          following purposes: To confirm your identity and grant you access to
+          your account To enhance the security of our application To communicate
+          with you regarding your account, including password resets and
+          account-related notifications We do not use your email address for any
+          other purposes, such as marketing or third-party sharing. <br />
+          4. Data Security We take the security of your information seriously
+          and implement appropriate measures to protect it from unauthorized
+          access, disclosure, alteration, or destruction. While we take these
+          steps to ensure the security of your data, please be aware that no
+          online platform can guarantee 100% security.
+          <br /> 5. Third-Party Access We do not share your email address or any
+          other personal information with third parties, except where required
+          by law or when necessary to provide the services you have requested.{' '}
+          <br />
+          6. Cookies and Tracking Technologies We may use cookies and other
+          tracking technologies to improve your experience on our website. You
+          can choose to accept or decline these cookies in your browser
+          settings. <br />
+          7. Changes to Privacy Policy We may update this Privacy Policy from
+          time to time to reflect changes in our practices or for other
+          operational, legal, or regulatory reasons. We will notify you of any
+          significant changes to this policy. 8. Contact Information If you have
+          any questions or concerns about this Privacy Policy or the way we
+          handle your personal information, please contact us at
+          contact@accelar.io.
+        </div>
+      </div>
     </section>
   )
 }
