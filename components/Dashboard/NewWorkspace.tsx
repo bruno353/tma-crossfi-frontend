76483e42f9e56f86233e83e70e3fe7cdd636b6d4
Bruno Santos <brunolsantos152@gmail.com>
2023-12-12 00:24:27 -0300

    feat: just adding new features

--- components/Dashboard/NewWorkspace.tsx
+++ components/Dashboard/NewWorkspace.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable dot-notation */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 'use client'
 // import { useState } from 'react'
-import { useEffect, useState, ChangeEvent, FC, useContext } from 'react'
+import { useEffect, useState, ChangeEvent, FC, useContext, useRef } from 'react'
 import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 import { useForm, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -18,9 +19,14 @@ import 'react-datepicker/dist/react-datepicker.css'
 
 const NewWorkspaceModal = ({ isOpen, onClose }) => {
   const [workspaceName, setWorkspaceName] = useState('')
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const fileInputRef = useRef(null)
 
   const handleInputChange = (e) => {
-    setWorkspaceName(e.target.value)
+    if (!isLoading) {
+      setWorkspaceName(e.target.value)
+    }
   }
 
   const handleOverlayClick = () => {
@@ -33,10 +39,29 @@ const NewWorkspaceModal = ({ isOpen, onClose }) => {
     e.stopPropagation()
   }
 
+  const handleFileChange = (e) => {
+    if (isLoading) return
+
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      setSelectedImage(reader.result)
+    }
+    reader.readAsDataURL(file)
+  }
+
+  const handleImageClick = () => {
+    fileInputRef.current.click()
+  }
+
+  const handleCreateWorkspace = async () => {
+    setIsLoading(true)
+  }
+
   return (
     <div
       onClick={handleOverlayClick}
-      className={`fixed inset-0 z-50 flex items-center justify-center ${
+      className={`fixed  inset-0 z-50 flex items-center justify-center font-normal ${
         isOpen ? 'visible opacity-100' : 'invisible opacity-0'
       } transition-opacity duration-300`}
     >
@@ -44,10 +69,71 @@ const NewWorkspaceModal = ({ isOpen, onClose }) => {
         onClick={handleModalClick}
         className="absolute inset-0 bg-black opacity-50"
       ></div>
-      <div className="z-50 rounded-md bg-white p-8" onClick={handleModalClick}>
-        <h2 className="mb-4 text-2xl">New Workspace</h2>
+      <div
+        className="relative z-50 w-[250px] rounded-md bg-[#060621] p-8 md:w-[500px]"
+        onClick={handleModalClick}
+      >
+        <div
+          onClick={onClose}
+          className="absolute right-5 top-2 cursor-pointer text-[18px] font-light"
+        >
+          x
+        </div>
+        <h2 className="mb-6 text-2xl">New Workspace</h2>
+        <div>
+          <label
+            htmlFor="workspaceName"
+            className="mb-2 block text-[14px] text-[#C5C4C4]"
+          >
+            Workspace logo
+          </label>
+        </div>
+        <div
+          className="groupLogo group relative mb-6 h-fit w-[90px] cursor-pointer"
+          onClick={handleImageClick}
+        >
+          {selectedImage ? (
+            <>
+              <img
+                src={selectedImage}
+                className="w-[90px] rounded-full transition duration-300 ease-in-out group-hover:opacity-10"
+              />
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 transition duration-300 ease-in-out group-hover:opacity-100">
+                <img
+                  src="/images/dashboard/pencil.svg"
+                  className="mb-6 w-[50px] rounded-full"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <img
+                src="/images/dashboard/work.webp"
+                className="w-[90px] rounded-full transition duration-300 ease-in-out group-hover:opacity-10"
+              />
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 transition duration-300 ease-in-out group-hover:opacity-100">
+                <img
+                  src="/images/dashboard/pencil.svg"
+                  className="mb-6 w-[50px] rounded-full"
+                />
+              </div>
+            </>
+          )}
+
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            accept=".png, .jpg, .jpeg, .svg, .webp"
+            style={{ display: 'none' }}
+          />
+        </div>
+
         <div className="mb-4">
-          <label htmlFor="workspaceName" className="text-gray-700 block">
+          <label
+            htmlFor="workspaceName"
+            className="mb-2 block text-[14px] text-[#C5C4C4]"
+          >
             Workspace name
           </label>
           <input
@@ -56,27 +142,25 @@ const NewWorkspaceModal = ({ isOpen, onClose }) => {
             name="workspaceName"
             value={workspaceName}
             onChange={handleInputChange}
-            className="border-gray-300 w-full rounded-md border px-3 py-2"
+            className="w-full rounded-md border border-transparent px-6 py-1 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
           />
         </div>
-        <div className="flex justify-end">
-          <button
-            onClick={onClose}
-            className="bg-red-500 hover:bg-red-600 rounded-md px-4 py-2 text-white"
-          >
-            Close
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-600 ml-2 rounded-md px-4 py-2 text-white"
+        <div className="mt-10 flex justify-start">
+          <div
+            className={`${
+              isLoading
+                ? 'animate-pulse bg-[#8e68e829] '
+                : 'cursor-pointer  hover:bg-[#8e68e829]'
+            }  rounded-[5px] border-[1px] border-[#642EE7] p-[2px] px-[10px] text-[14px] text-[#642EE7] `}
             onClick={() => {
               // Add your logic here to create a new workspace
               // You can use workspaceName state to get the entered name
               // Close the modal when done
-              onClose()
+              handleCreateWorkspace()
             }}
           >
             Create
-          </button>
+          </div>
         </div>
       </div>
     </div>
--- utils/api.tsx
+++ utils/api.tsx
@@ -191,3 +191,28 @@ export async function getCurrentUser(userSessionToken: string) {
 
   return finalData
 }
+
+export async function createWorkspace(data: any, userSessionToken: string) {
+  const config = {
+    method: 'post' as 'post',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/workspace/functions/createWorkspace`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+      'Content-Type': 'multipart/form-data',
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
