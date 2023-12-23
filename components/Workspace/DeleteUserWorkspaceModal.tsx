2998811cfa4cef8ccdff1420ae92ffb3b75b420e
Bruno Santos <brunolsantos152@gmail.com>
2023-12-23 16:08:45 -0300

    feat: new isUserAdmin logic

--- components/Workspace/DeleteUserWorkspaceModal.tsx
+++ components/Workspace/DeleteUserWorkspaceModal.tsx
@@ -24,7 +24,7 @@ const DeleteUserWorkspaceModal = ({ userWorkspace, onUpdateM }: MenuI) => {
 
     const { userSessionToken } = parseCookies()
     const data = {
-      id: userWorkspace.id,
+      id: userWorkspaceId,
     }
 
     try {
