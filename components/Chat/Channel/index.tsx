3a52bf4a7e7ea02f0612f34bd118e0c39910b3f9
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-03 22:39:09 -0300

    feat: new deployment of chats

---
+++ components/Chat/Channel/index.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/prefer-as-const */
+/* eslint-disable dot-notation */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+'use client'
+// import { useState } from 'react'
+import { useEffect, useState, ChangeEvent, FC, useContext } from 'react'
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Eye, EyeSlash } from 'phosphor-react'
+import * as Yup from 'yup'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import 'react-quill/dist/quill.snow.css' // import styles
+import 'react-datepicker/dist/react-datepicker.css'
+import { getWorkspace } from '@/utils/api'
+import nookies, { parseCookies, setCookie } from 'nookies'
+import { getUserChannels } from '@/utils/api-chat'
+
+const Channel = (id: any) => {
+  const { push } = useRouter()
+
+  //   useEffect(() => {
+  //     window.scrollTo({
+  //       top: 0,
+  //       behavior: 'smooth',
+  //     })
+  //     if (id) {
+  //       getData()
+  //     } else {
+  //       push('/dashboard')
+  //     }
+  //   }, [id])
+  return (
+    <>
+      <div className="relative z-10 flex h-screen w-fit overflow-hidden  px-[20px] pb-16 pt-5 text-[16px] text-[#C5C4C4] md:pb-20 lg:mt-[100px] lg:pb-28 2xl:pr-[30px] 2xl:text-[18px]">
+        oiiii
+      </div>
+    </>
+  )
+}
+
+export default Channel
--- components/Chat/ChatSidebar/index.tsx
+++ components/Chat/ChatSidebar/index.tsx
@@ -108,101 +108,106 @@ const ChatSidebar = (id: any) => {
   return (
     <>
       <div className="relative z-10 flex h-screen w-fit overflow-hidden bg-[#33323E] px-[20px] pb-16 pt-5 text-[16px] text-[#C5C4C4] md:pb-20 lg:mt-[100px] lg:pb-28 2xl:pr-[30px] 2xl:text-[18px]">
-        <div className="text-[12px] font-light  2xl:text-[14px]">
-          {channelOption.map((option, index) => (
-            <div key={index} className="mb-[30px]">
-              <div className={`mb-1 flex min-w-[150px] items-center`}>
-                <div
-                  onClick={() => {
-                    handleSidebarClick(option.type)
-                  }}
-                  className="flex cursor-pointer gap-x-[5px] hover:text-[#fff]"
-                >
-                  <img
-                    src="/images/chat/arrow-down.svg"
-                    alt="image"
-                    className={`w-[8px]  ${
-                      !sidebarOption[option.type] && '-rotate-90'
-                    }`}
-                  />
-                  <div>{option.name}</div>
-                </div>
-
-                {workspace?.isUserAdmin && (
+        <div>
+          <div className="text-[12px] font-light  2xl:text-[14px]">
+            {channelOption.map((option, index) => (
+              <div key={index} className="mb-[30px]">
+                <div className={`mb-1 flex min-w-[150px] items-center`}>
                   <div
                     onClick={() => {
-                      setIsCreatingNewChannel(true)
-                      setIsCreatingNewChannelType(option.type)
+                      handleSidebarClick(option.type)
                     }}
-                    className="ml-auto cursor-pointer text-[17px] font-normal hover:text-[#fff]"
+                    className="flex cursor-pointer gap-x-[5px] hover:text-[#fff]"
                   >
-                    +
+                    <img
+                      src="/images/chat/arrow-down.svg"
+                      alt="image"
+                      className={`w-[8px]  ${
+                        !sidebarOption[option.type] && '-rotate-90'
+                      }`}
+                    />
+                    <div>{option.name}</div>
                   </div>
-                )}
-              </div>
-              {sidebarOption[option.type] &&
-                channels &&
-                channels?.length > 0 && (
-                  <div>
-                    {channels?.map((optionChannel, index) => (
-                      <div key={index}>
-                        {option.type === optionChannel.type && (
-                          <div className="mb-[7px] flex cursor-pointer gap-x-[3px] 2xl:gap-x-[3px]">
-                            <img
-                              src={channelTypeToLogo[option.type]}
-                              alt="image"
-                              className={'w-[14px] 2xl:w-[16px]'}
-                            />
-                            <div
-                              title={optionChannel.name}
-                              className="max-w-[180px] overflow-hidden truncate text-ellipsis whitespace-nowrap hover:text-[#fff] 2xl:max-w-[200px]"
-                            >
-                              {' '}
-                              {optionChannel.name}{' '}
-                            </div>
-                            {optionChannel.isPrivate && (
-                              <img
-                                src={'/images/chat/lock.svg'}
-                                alt="image"
-                                className={'w-[12px] 2xl:w-[14px]'}
-                              />
-                            )}
-                          </div>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                )}
-            </div>
-          ))}
-        </div>
-        <div className="mt-[30px] grid gap-y-[25px] 2xl:mt-[40px]">
-          {users?.map((workspaceUser, index) => (
-            <div
-              key={index}
-              className="flex items-center gap-x-[10px] text-[15px] font-normal"
-            >
-              <div
-                onMouseEnter={() => setIsUserModalOpen(workspaceUser.id)}
-                onMouseLeave={() => setIsUserModalOpen(null)}
-                className="relative flex items-center gap-x-[10px]"
-              >
-                <img
-                  alt="ethereum avatar"
-                  src={workspaceUser.user.profilePicture}
-                  className="w-[35px] rounded-full"
-                ></img>
-                <div className="w-[350px] overflow-hidden truncate text-ellipsis whitespace-nowrap">
-                  {workspaceUser.user.email}
+
+                  {workspace?.isUserAdmin && (
+                    <div
+                      onClick={() => {
+                        setIsCreatingNewChannel(true)
+                        setIsCreatingNewChannelType(option.type)
+                      }}
+                      className="ml-auto cursor-pointer text-[17px] font-normal hover:text-[#fff]"
+                    >
+                      +
+                    </div>
+                  )}
                 </div>
-                {isUserModalOpen === workspaceUser.id && (
-                  <div className="absolute -top-[10px] -translate-y-[100%] ">
-                    <UserWorkspaceInfoModal userWorkspace={workspaceUser} />
-                  </div>
-                )}
+                {sidebarOption[option.type] &&
+                  channels &&
+                  channels?.length > 0 && (
+                    <div>
+                      {channels?.map((optionChannel, index) => (
+                        <div key={index}>
+                          {option.type === optionChannel.type && (
+                            <div className="mb-[7px] flex cursor-pointer gap-x-[3px] 2xl:gap-x-[3px]">
+                              <img
+                                src={channelTypeToLogo[option.type]}
+                                alt="image"
+                                className={'w-[14px] 2xl:w-[16px]'}
+                              />
+                              <div
+                                title={optionChannel.name}
+                                className="max-w-[180px] overflow-hidden truncate text-ellipsis whitespace-nowrap hover:text-[#fff] 2xl:max-w-[200px]"
+                              >
+                                {' '}
+                                {optionChannel.name}{' '}
+                              </div>
+                              {optionChannel.isPrivate && (
+                                <img
+                                  src={'/images/chat/lock.svg'}
+                                  alt="image"
+                                  className={'w-[12px] 2xl:w-[14px]'}
+                                />
+                              )}
+                            </div>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  )}
               </div>
+            ))}
+          </div>
+          <div className="mt-[30px] grid gap-y-[5px] 2xl:mt-[40px]">
+            <div className="mb-[10px] text-[12px]  font-light 2xl:text-[14px]">
+              Members
             </div>
-          ))}
+            {workspace?.UserWorkspace?.map((workspaceUser, index) => (
+              <div
+                key={index}
+                className="flex cursor-pointer items-center gap-x-[10px] rounded-md p-[10px] text-[15px] font-normal transition hover:bg-[#24232e63]"
+              >
+                <div
+                  onMouseEnter={() => setIsUserModalOpen(workspaceUser.id)}
+                  onMouseLeave={() => setIsUserModalOpen(null)}
+                  className="relative flex items-center gap-x-[10px]"
+                >
+                  <img
+                    alt="ethereum avatar"
+                    src={workspaceUser.user.profilePicture}
+                    className="w-[35px] rounded-full"
+                  ></img>
+                  <div className="w-[250px] overflow-hidden truncate text-ellipsis whitespace-nowrap">
+                    {workspaceUser.user.email}
+                  </div>
+                  {isUserModalOpen === workspaceUser.id && (
+                    <div className="absolute -top-[10px] -translate-y-[100%] ">
+                      <UserWorkspaceInfoModal userWorkspace={workspaceUser} />
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <NewChannelModal
