4fd1e1ca90720b102c7a9181f15ee836f729a485
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-06 23:08:12 -0300

    feat: removing unused variables

--- app/change-password-recovery/[id]/page.tsx
+++ app/change-password-recovery/[id]/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import ChangePasswordRecovery from '@/components/ChangePasswordRecovery'
 import ChangePasswordRecoveryFinal from '@/components/ChangePasswordRecovery2'
 import ScrollUp from '@/components/Common/ScrollUp'
-import { Inter } from '@next/font/google'
-
-// eslint-disable-next-line no-unused-vars
-const inter = Inter({ subsets: ['latin'] })
 
 export default function UserPage({ params }) {
   console.log(params.id)
--- app/change-password/page.tsx
+++ app/change-password/page.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable no-unused-vars */
 import ChangePassword from '@/components/ChangePassword'
 import ScrollUp from '@/components/Common/ScrollUp'
-import { Inter } from '@next/font/google'
-// import NewTask from '@/components/NewTask'
-
-// eslint-disable-next-line no-unused-vars
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Tasks() {
   return (
