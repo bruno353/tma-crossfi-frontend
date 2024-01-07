74863a8d4ea4ef9d188550655d802b743848d1bb
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-06 23:09:39 -0300

    feat: removing unused variables

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Accelar App</title>
+      <title>Accelar plataform</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Accelar App" />
       <link rel="icon" href="/images/logo-icon.png" />
--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable no-unused-vars */
 import ScrollUp from '@/components/Common/ScrollUp'
-import Faqs from '@/components/FAQS'
-import Login from '@/components/Login'
 import PrivacyPolicy from '@/components/PrivacyPolicy'
-import { Inter } from '@next/font/google'
-// import NewTask from '@/components/NewTask'
-
-// eslint-disable-next-line no-unused-vars
-const inter = Inter({ subsets: ['latin'] })
 
 export default function FaqsE() {
   return (
