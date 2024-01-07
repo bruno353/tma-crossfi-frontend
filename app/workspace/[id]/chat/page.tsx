1edaf3947682e54ae3a5a1d80bc0402d2d3b1940
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-06 23:07:36 -0300

    feat: removing unused var

--- app/workspace/[id]/chat/page.tsx
+++ app/workspace/[id]/chat/page.tsx
@@ -5,12 +5,6 @@ import ChangePasswordRecoveryFinal from '@/components/ChangePasswordRecovery2'
 import Chat from '@/components/Chat'
 import Start from '@/components/Chat/Start'
 import ScrollUp from '@/components/Common/ScrollUp'
-import Dashboard from '@/components/Dashboard'
-import Workspace from '@/components/Workspace'
-import { Inter } from '@next/font/google'
-
-// eslint-disable-next-line no-unused-vars
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Page({ params }) {
   console.log(params.id)
