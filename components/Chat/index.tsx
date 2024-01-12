8e308710ef8d51fe894e47612eb8837709a0a0dd
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-12 10:51:19 -0300

    feat: removing unused chat

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -6,12 +6,6 @@
 // import { useState } from 'react'
 import { useEffect, useState, ChangeEvent, FC, useContext } from 'react'
 import { usePathname, useSearchParams, useRouter } from 'next/navigation'
-import { useForm, Controller } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { Eye, EyeSlash } from 'phosphor-react'
-import * as Yup from 'yup'
-import axios from 'axios'
-import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import 'react-quill/dist/quill.snow.css' // import styles
 import 'react-datepicker/dist/react-datepicker.css'
