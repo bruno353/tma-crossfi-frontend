106a789d4523f84cf3b6fcc6fefd4417df57b29a
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-13 21:54:16 -0300

    feat: new endpoint read channel

--- utils/api-chat.tsx
+++ utils/api-chat.tsx
@@ -127,3 +127,27 @@ export async function editMessage(data: any, userSessionToken: string) {
 
   return finalData
 }
+
+export async function readChannel(data: any, userSessionToken: string) {
+  const config = {
+    method: 'put' as 'put',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/chat/functions/readChannel`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
