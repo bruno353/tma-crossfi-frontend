6ef6ad455a7ae808772711029a786b58a9d0eff5
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-26 11:38:54 -0300

    fix: ui fix

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -108,12 +108,12 @@ const SignUp = () => {
 
   return (
     <>
-      <section className="relative z-10 overflow-hidden pb-16 pt-2 md:pb-20 lg:pb-28 lg:pt-[20px]">
+      <section className="relative z-10 overflow-hidden pb-2 pt-2 lg:pt-[0px]">
         <div className="container">
           <form onSubmit={handleSubmit(onSubmit)} className="">
             <div className="-mx-4 flex flex-wrap">
               <div className="w-full px-4">
-                <div className="mx-auto max-w-[500px] rounded-md bg-primary bg-opacity-5 px-6 py-10 dark:bg-dark sm:p-[60px]">
+                <div className="mx-auto max-w-[500px] rounded-md bg-primary bg-opacity-5 px-6 py-10 dark:bg-dark sm:px-[60px]">
                   <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
                     Sign in to your account
                   </h3>
--- components/Signup/index.tsx
+++ components/Signup/index.tsx
@@ -129,7 +129,7 @@ const SignUp = () => {
 
   return (
     <>
-      <section className="relative z-10 overflow-hidden pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[180px]">
+      <section className="relative z-10 overflow-hidden pb-4 pt-2 lg:pt-[0px]">
         <div className="container">
           <form onSubmit={handleSubmit(onSubmit)} className="">
             <div className="-mx-4 flex flex-wrap">
