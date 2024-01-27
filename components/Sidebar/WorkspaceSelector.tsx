ca0d22630c927354171fe9dd5359f9126d40c409
Bruno Laureano <brunolsantos152@gmail.com>
2024-01-26 23:58:22 -0300

    feat: ui fix workspace

--- components/Sidebar/WorkspaceSelector.tsx
+++ components/Sidebar/WorkspaceSelector.tsx
@@ -23,7 +23,7 @@ const WorkspaceSelector = ({ user }: MenuI) => {
       <div className="h-full w-[300px] rounded-[10px]  border-[1px] border-[#33323e] bg-[#060621] p-[15px] text-[14px] font-normal text-[#c5c4c4]">
         <div className="grid gap-y-[20px]">My workspaces</div>
         <div className="my-[7px] h-[1px] w-full bg-[#33323e]"></div>
-        <div className="max-h-[100px] overflow-y-auto">
+        <div className="max-h-[250px] overflow-y-auto scrollbar-thin scrollbar-track-[#1D2144] scrollbar-thumb-[#c5c4c4] scrollbar-track-rounded-md scrollbar-thumb-rounded-md">
           {finalWorkspaces.map((workspace, index) => (
             <Link key={index} href={`/workspace/${workspace.id}`}>
               <div className="flex cursor-pointer items-center gap-x-[12px] rounded-[5px] p-[5px] hover:bg-[#c5c5c510]">
@@ -44,19 +44,14 @@ const WorkspaceSelector = ({ user }: MenuI) => {
           ))}
         </div>
 
-        <div className="my-[20px] h-[0.5px] w-full bg-[#33323e]"></div>
+        <div className="my-[7px] h-[0.5px] w-full bg-[#33323e]"></div>
         <div
           onClick={() => {
             // onSignOut()
           }}
-          className="flex w-fit cursor-pointer items-center gap-x-[12px] rounded-[5px] p-[5px] hover:bg-[#c5c5c510]"
+          className="flex w-fit cursor-pointer items-center gap-x-[12px] rounded-[5px] p-[5px] text-[12px] hover:bg-[#c5c5c510]"
         >
-          <div className="text-[#cc5563]">Sign out</div>
-          <img
-            alt="ethereum avatar"
-            src="/images/header/logout.svg"
-            className="w-[18px] rounded-full"
-          ></img>
+          <div className="text-[#c5c4c4]">+ New workspace</div>
         </div>
       </div>
     </>
