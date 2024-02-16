e1be4713f25010fd1e2bfd1dd9d6165338c47f51
Bruno Santos <brunolsantos152@gmail.com>
2024-02-16 09:23:48 -0300

    feat: new api and type automation

---
+++ types/automation.ts
@@ -0,0 +1,19 @@
+// export interface LLMInstanceProps {
+//   id: string
+//   url: string
+//   name: string
+//   typeTemplate: string
+//   workspaceId: string
+//   llmAppId: string
+//   // eslint-disable-next-line no-use-before-define
+//   llmApp: LLMAppProps
+//   createdAt: string
+//   updatedAt: string
+// }
+
+export interface AutomationWorkflowProps {
+  id: string
+  name: string
+  createdAt: string
+  updatedAt: string
+}
---
+++ utils/api-automation.tsx
@@ -0,0 +1,144 @@
+/* eslint-disable @typescript-eslint/prefer-as-const */
+'use client'
+import {
+  EmailRecoverPassword,
+  RecoverPassword,
+  SigninForm,
+  SignupForm,
+} from '@/types/user'
+import axios from 'axios'
+
+export async function createAutomationWorkflow(
+  data: any,
+  userSessionToken: string,
+) {
+  const config = {
+    method: 'post' as 'post',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/automation/functions/createWorkflow`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
+
+export async function editAutomationWorkflow(
+  data: any,
+  userSessionToken: string,
+) {
+  const config = {
+    method: 'put' as 'put',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/automation/functions/editWorkflow`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
+
+export async function deleteAutomationWorkflow(
+  data: any,
+  userSessionToken: string,
+) {
+  const config = {
+    method: 'delete' as 'delete',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/automation/functions/deleteWorkflow`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
+
+export async function getAutomationWorkflows(
+  data: any,
+  userSessionToken: string,
+) {
+  const config = {
+    method: 'post' as 'post',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/automation/functions/getWorkflows`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
+
+export async function getAutomationWorkflow(
+  data: any,
+  userSessionToken: string,
+) {
+  const config = {
+    method: 'post' as 'post',
+    url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/automation/functions/getWorkflow`,
+    headers: {
+      'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
+      'X-Parse-Session-Token': userSessionToken,
+    },
+    data,
+  }
+
+  let finalData
+
+  await axios(config).then(function (response) {
+    if (response.data) {
+      finalData = response.data
+      console.log('api response')
+      console.log(finalData)
+    }
+  })
+
+  return finalData
+}
