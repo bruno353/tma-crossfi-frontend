358dd7c39b7528616fe503897f54940b40f66ad8
Bruno Laureano <brunolsantos152@gmail.com>
2024-11-27 13:23:44 -0300

    feat: adding herusec on commit workflow analysis

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -1,31 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable dot-notation */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-unused-vars */
 'use client'
-// import { useState } from 'react'
-import { useEffect, useState, ChangeEvent, FC, useContext } from 'react'
-import { usePathname, useSearchParams, useRouter } from 'next/navigation'
-import { useForm, Controller } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { Eye, EyeSlash, SmileySad } from 'phosphor-react'
-import * as Yup from 'yup'
-import axios from 'axios'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import dynamic from 'next/dynamic'
-import 'react-quill/dist/quill.snow.css' // import styles
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+import { useEffect, useState } from 'react'
 import { parseCookies } from 'nookies'
-import { AccountContext } from '../../contexts/AccountContext'
-import Link from 'next/link'
-import NewWorkspaceModal from './NewWorkspace'
-import { getUserWorkspace } from '@/utils/api'
-import { WorkspaceProps } from '@/types/workspace'
-import { Logo } from '../Sidebar/Logo'
-import { workflowTypeToOptions } from '@/utils/consts'
+import { toast } from 'react-toastify'
+import { SmileySad } from 'phosphor-react'
 import { callAxiosBackend } from '@/utils/general-api'
 import { TelegramAppProps, TelegramAppType } from '@/types/telegram'
 
@@ -34,18 +11,8 @@ const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [apps, setApps] = useState<TelegramAppProps[]>([])
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
-  const [queryInput, setQueryInput] = useState<string>()
-
-  const { push } = useRouter()
-  const pathname = usePathname()
-
-  const openModal = () => {
-    setIsCreatingNewWorkspace(true)
-  }
-
-  const closeModal = () => {
-    setIsCreatingNewWorkspace(false)
-  }
+  const [queryInput, setQueryInput] = useState<string>('') // Estado do input
+  const [debouncedQuery, setDebouncedQuery] = useState<string>('') // Estado do debounce
 
   const categoryToList = {
     Web3: TelegramAppType.WEB3,
@@ -53,29 +20,53 @@ const Dashboard = () => {
     Utilities: TelegramAppType.UTILITY,
   }
 
-  const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category)
-    getData(category)
+  // Atualiza o valor de debounce após um atraso
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedQuery(queryInput)
+    }, 500) // Atraso de 500ms
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [queryInput])
+
+  // Busca os dados quando o valor de debounce ou categoria muda
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!debouncedQuery && !selectedCategory) return // Evita buscas desnecessárias
+      const { userSessionToken } = parseCookies()
+      setIsLoading(true)
+
+      try {
+        const res = await callAxiosBackend(
+          'get',
+          `/telegram/apps?chain=CROSSFI${
+            selectedCategory ? `&category=${selectedCategory}` : ''
+          }${debouncedQuery ? `&search=${debouncedQuery}` : ''}`,
+          userSessionToken,
+        )
+        setApps(res)
+      } catch (err) {
+        console.error(err)
+        toast.error(
+          `Error: ${err.response?.data?.message || 'Failed to fetch data'}`,
+        )
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [debouncedQuery, selectedCategory])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsLoading(true) // Inicia o loading imediatamente
+    setQueryInput(e.target.value)
   }
 
-  async function getData(category: string | null = null) {
-    const { userSessionToken } = parseCookies()
-
-    try {
-      const res = await callAxiosBackend(
-        'get',
-        `/telegram/apps?chain=CROSSFI${
-          category ? `&category=${category}` : ''
-        }`,
-        userSessionToken,
-      )
-      setApps(res)
-      setIsLoading(false)
-    } catch (err) {
-      console.log(err)
-      toast.error(`Error: ${err.response.data.message}`)
-      setIsLoading(false)
-    }
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category)
   }
 
   function NoWorkspaces() {
@@ -87,85 +78,71 @@ const Dashboard = () => {
     )
   }
 
-  const handleInputChange = (e) => {
-    if (!isLoading) {
-      setQueryInput(e.target.value)
-    }
-  }
-
-  useEffect(() => {
-    setIsLoading(true)
-    getData()
-  }, [])
-
   return (
-    <>
-      <section className="relative z-10 overflow-hidden px-[5px] pb-16 text-[16px] md:pb-20 lg:pb-28 lg:pt-[40px]">
-        <div className="container text-[#fff]">
-          <div className="mb-4 flex gap-4">
-            {Object.keys(categoryToList).map((category) => (
-              <button
-                key={category}
-                onClick={() => handleCategoryClick(categoryToList[category])}
-                className={`rounded px-1 py-2 ${
-                  selectedCategory === categoryToList[category]
-                    ? 'bg-blue-500'
-                    : 'bg-gray-700'
-                } text-white`}
-              >
-                {category}
-              </button>
-            ))}
-          </div>
-          <div className="">
-            <input
-              type="text"
-              id="workspaceName"
-              name="workspaceName"
-              maxLength={100}
-              placeholder="Search apps"
-              value={queryInput}
-              onChange={handleInputChange}
-              className="w-[300px] rounded-md border border-transparent px-6 py-1 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp md:w-[400px]"
-            />
-          </div>
-          <div className="mb-3 mt-[50px] font-semibold">Community choices</div>
-          <div className=" overflow-x-auto whitespace-nowrap">
-            {apps.map((app, index) => (
-              <a key={index} href={app.telegramUrl} className="inline-block">
-                <div className="relative inline-block cursor-pointer rounded-[5px] px-3  text-center text-xs text-[#fff] hover:bg-[#13132c]">
-                  <img
-                    src={app.logoUrl}
-                    alt="image"
-                    className={`h-[60px] w-[60px] rounded-lg`}
-                  />
-                  <div
-                    title={app.name}
-                    className="mt-1 max-w-[60px] overflow-hidden truncate text-ellipsis whitespace-nowrap"
-                  >
-                    {app.name}
-                  </div>
-                </div>
-              </a>
-            ))}
-          </div>
-          {isLoading && (
-            <div className="flex w-full justify-between gap-x-[30px]">
-              <div className="h-40 w-full animate-pulse rounded-[5px] bg-[#1d2144b0]"></div>
-              <div className="h-40 w-full animate-pulse rounded-[5px] bg-[#1d2144b0]"></div>
-              <div className="h-40 w-full animate-pulse rounded-[5px] bg-[#1d2144b0]"></div>
-            </div>
-          )}
-          {apps?.length === 0 && !isLoading && (
-            <div className="mt-[100px] w-full items-center">
-              {NoWorkspaces()}
-            </div>
-          )}
+    <section className="relative z-10 overflow-hidden px-[5px] pb-16 text-[16px] md:pb-20 lg:pb-28 lg:pt-[40px]">
+      <div className="container text-[#fff]">
+        <div className="mb-4 flex gap-4">
+          {Object.keys(categoryToList).map((category) => (
+            <button
+              key={category}
+              onClick={() => handleCategoryClick(categoryToList[category])}
+              className={`rounded px-1 py-2 ${
+                selectedCategory === categoryToList[category]
+                  ? 'bg-blue-500'
+                  : 'bg-gray-700'
+              } text-white`}
+            >
+              {category}
+            </button>
+          ))}
         </div>
-      </section>
-
-      <NewWorkspaceModal isOpen={isCreatingNewWorkspace} onClose={closeModal} />
-    </>
+        <div className="relative">
+  <img
+    alt="search"
+    src="/images/telegram/search.svg"
+    className="absolute left-3 top-1/2 w-[20px] -translate-y-1/2 text-gray-400"
+  />
+  <input
+    type="text"
+    id="workspaceName"
+    name="workspaceName"
+    maxLength={100}
+    placeholder="Search apps"
+    value={queryInput}
+    onChange={handleInputChange}
+    className="w-full rounded-md border border-transparent pl-10 pr-4 py-2 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp md:w-[400px]"
+  />
+</div>
+        <div className="mb-3 mt-[50px] font-semibold">Community choices</div>
+        <div className="overflow-x-auto whitespace-nowrap">
+          {apps.map((app, index) => (
+            <a key={index} href={app.telegramUrl} className="inline-block">
+              <div className="relative inline-block cursor-pointer rounded-[5px] px-3 text-center text-xs text-[#fff] hover:bg-[#13132c]">
+                <img
+                  src={app.logoUrl}
+                  alt="image"
+                  className="h-[60px] w-[60px] rounded-lg"
+                />
+                <div
+                  title={app.name}
+                  className="mt-1 max-w-[60px] overflow-hidden truncate text-ellipsis whitespace-nowrap"
+                >
+                  {app.name}
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+        {isLoading && (
+          <div className="flex w-full justify-between gap-x-[30px]">
+            <div className="h-40 w-full animate-pulse rounded-[5px] bg-[#1d2144b0]"></div>
+            <div className="h-40 w-full animate-pulse rounded-[5px] bg-[#1d2144b0]"></div>
+            <div className="h-40 w-full animate-pulse rounded-[5px] bg-[#1d2144b0]"></div>
+          </div>
+        )}
+        {apps?.length === 0 && !isLoading && <NoWorkspaces />}
+      </div>
+    </section>
   )
 }
 
